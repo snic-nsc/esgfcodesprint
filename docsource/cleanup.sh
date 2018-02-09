45b41782ef0749c4a69355904768e6ba59cad68c
Prashanth Dwarakanath <pchengi@nsc.liu.se>
2018-02-09 17:24:56 +0100

    update documentation

---
+++ docsource/cleanup.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rm -f checklist.aux checklist.bcf checklist.log checklist.out checklist.run.xml checklist.toc
---
+++ scripts/rinseandrepeat.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+majver=2.6
+subver=4
+devel=0
+
+if [ ! -s /usr/local/bin/esg-purge.sh ]; then
+    echo "No esg-purge.sh file found; possibly nothing to purge.";
+    exit 0;
+fi
+
+source /usr/local/bin/esg-purge.sh && esg-purge all && rm -f /etc/yum.repos.d/esgf.repo;
+
+while [ 1 ]; do
+    read -p "Major version: $majver, subversion $subver, devel=$devel. If you want to change, press n, any other key to continue " choice;
+    if [ "$choice" != 'n' ]; then
+        break;
+    fi
+    read -p "Enter major version " majver
+    read -p "Enter subversion " subver
+    read -p "Enter devel " devel
+done
+
+sed "s/develval/$devel/" fetchbootstrap.tmpl >fetchbootstrap.sh
+sed -i "s/rmversion/$majver/" fetchbootstrap.sh
+sed -i "s/rsversion/$subver/" fetchbootstrap.sh
+bash fetchbootstrap.sh
+
+if [ ! -s esg-autoinstall.conf ]; then
+    echo "No local copy of esg-autoinstall.conf found. Copy /usr/local/etc/esg-autoinstall.conf here, modify it with values suitable to you, and try again.";
+    exit -1;
+fi
+cp esg-autoinstall.conf /usr/local/etc/esg-autoinstall.conf
+echo "You are all set. If you have the right values setup in esg-autoinstall.conf, you can execute the following:"
+echo "script -c '/usr/local/bin/esg-autoinstall' installation.log
