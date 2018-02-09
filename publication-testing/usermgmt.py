8cdde84fc8fce5dfc5e63700994e133225927932
Prashanth Dwarakanath <pchengi@nsc.liu.se>
2018-02-09 12:14:34 +0100

    update usermgmt.py

--- publication-testing/usermgmt.py
+++ publication-testing/usermgmt.py
@@ -5,7 +5,7 @@ import psycopg2
 import sys
 import readline
 
-with open('publisher_user_pass') as passfile:
+with open('/esg/config/.esg_pg_pass') as passfile:
     passwd=passfile.readline().split('\n')[0]
 try:
     conn=psycopg2.connect("dbname='esgcet' user='dbsuper' password='%s'"%(passwd))
@@ -29,22 +29,35 @@ if enablepub != None:
             enablepub=raw_input("Enter the value of the idp server, e.g esg-idx.demonet.local\n")
         else:
             break
-    qperm="insert into esgf_security.permission (user_id,group_id,role_id,approved) values ( (select u.id from esgf_security.user u where u.openid='https://%s/esgf-idp/openid/testpub'), (select g.id from esgf_security.group g where g.name='ext_pub'), (select r.id from esgf_security.role r where r.name='user'), True )"%enablepub
+    qperm="insert into esgf_security.permission (user_id,group_id,role_id,approved) values ( (select u.id from esgf_security.user u where u.openid='https://%s/esgf-idp/openid/testpub'), (select g.id from esgf_security.group g where g.name='ext_pub'), (select r.id from esgf_security.role r where r.name='user'), True )"%(enablepub)
     qaddgp="insert into esgf_security.group(name,description,visible,automatic_approval) values ('ext_pub','external publishers group', False, False)"
     qadduser="insert into esgf_security.user(firstname,lastname,email,username,password,openid,organization,city,country) values('Publisher','Tester','pt@example.org','testpub','$1$GnNDs7Ed$M5hLjBHVOTJGZv4TcNCFU0','https://%s/esgf-idp/openid/testpub','ABC','Linkoping','SWEDEN')"%enablepub
 
-    cur.execute(qaddgp)
-    conn.commit()
+    try:
+        cur.execute(qaddgp)
+        conn.commit()
+    except:
+        conn.rollback()
     try:
         cur.execute(qadduser)
         conn.commit()
+    except:
+        conn.rollback()
+    try:
         cur.execute(qperm)
         conn.commit()
+        print 'Added user and adjusted permissions accordingly'
     except:
-        print 'Already present'
+        print 'No changes needed'
         sys.exit(0)
 
 if disablepub != None:
+    while True:
+        choice=raw_input("You entered \"%s\" as the value for the idp. Is this correct?(Y/n) "%disablepub)
+        if choice == 'N' or choice == 'n':
+            disablepub=raw_input("Enter the value of the idp server, e.g esg-idx.demonet.local\n")
+        else:
+            break
     try:
         cur.execute("select id from esgf_security.user where openid = 'https://%s/esgf-idp/openid/testpub'"%disablepub)
         records=cur.fetchall()
@@ -58,9 +71,7 @@ if disablepub != None:
         cur.execute(querystr)
         cur.execute("delete from esgf_security.user where openid = 'https://%s/esgf-idp/openid/testpub'"%disablepub)
         conn.commit()
-        cur.execute("delete from esgf_security.group where id = %d"%gp)
-        conn.commit()
+        print 'Deleted user account and publication permissions for openid https://%s/esgf-idp/openid/testpub'%(disablepub)
     except:
-        print 'Already deleted'
-        raise
+        print 'Already deleted/not found'
         sys.exit(0)
