29c56b38e6a3167e9dfbb7957c9371d71f2f71ac
Prashanth Dwarakanath <pchengi@nsc.liu.se>
2018-05-22 16:23:14 +0200

    update publish script

--- publication-testing/publish.sh
+++ publication-testing/publish.sh
@@ -2,6 +2,6 @@ mkdir -p /root/.globus
 export ESGINI=/esg/config/esgcet/esg.ini
 myproxy-logon -s esg-idx.demonet.local -l testpub -o /root/.globus/certificate-file
 source /usr/local/conda/bin/activate esgf-pub
-esgprep mapfile --mapfile test.map --project cordex /esg/data/datapool1
-esgpublish --map mapfiles/test.map --project cordex --new-version `date +%Y%m%d` --service fileservice
-esgpublish --map mapfiles/test.map --project cordex --new-version `date +%Y%m%d` --noscan --thredds --publish --service fileservice
+esgmapfile --mapfile test.map --project cordex /esg/data/datapool1
+esgpublish --map mapfiles/test.map --project cordex --service fileservice
+esgpublish --map mapfiles/test.map --project cordex --noscan --thredds --publish --service fileservice
