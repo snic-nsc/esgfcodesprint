f764d07ae68a6504e008b8d50b6ff95975b0ea76
Prashanth Dwarakanath <pchengi@nsc.liu.se>
2018-05-24 10:54:42 +0200

    add tds import script

---
+++ publication-testing/tdsimport.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Check to make sure java version is correct
+JAVA='/usr/local/jdk1.8.0_162/bin/java'
+nodelist='esg-data.demonet.local'
+pushd /usr/local/tomcat/webapps/esg-search/WEB-INF/classes || exit -1;
+
+for node in $nodelist; do
+        echo $node;
+        $JAVA -Dlog4j.configuration=./log4j.xml -Djava.ext.dirs=../lib esg.search.publish.impl.PublishingServiceMain http://$node/thredds/catalog/catalog.xml '*' THREDDS true /tmp/solrpub_$node.log
+done
