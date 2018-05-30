34e53fd8afe24e73baa167af101718a1f355e7be
Prashanth Dwarakanath <pchengi@nsc.liu.se>
2018-05-30 10:51:10 +0200

    update publish.sh

--- publication-testing/publish.sh
+++ publication-testing/publish.sh
@@ -1,4 +1,6 @@
 mkdir -p /root/.globus
+rm -rf /root/.globus/certificates
+ln -s /etc/grid-security/certificates /root/.globus/certificates
 export ESGINI=/esg/config/esgcet/esg.ini
 myproxy-logon -s esg-idx.demonet.local -l testpub -o /root/.globus/certificate-file
 source /usr/local/conda/bin/activate esgf-pub
