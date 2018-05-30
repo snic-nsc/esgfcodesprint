43ac176ee05bc029e647554a40a065eecee7c610
Prashanth Dwarakanath <pchengi@nsc.liu.se>
2018-05-30 11:03:47 +0200

    update unpublish

--- publication-testing/unpublish.sh
+++ publication-testing/unpublish.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source /usr/local/conda/bin/activate esgf-pub
-esgunpublish --project cordex --map mapfiles/test.map --database-delete
+esgunpublish --project cordex --map mapfiles/test.map --database-delete --delete
