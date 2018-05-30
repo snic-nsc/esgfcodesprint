efa85d94c33604947e16747749e4531f9b0fc7aa
Prashanth Dwarakanath <pchengi@nsc.liu.se>
2018-05-30 11:01:44 +0200

    add oldpublish.sh

---
+++ publication-testing/oldpublish.sh
@@ -0,0 +1,8 @@
+mkdir -p /root/.globus
+export ESGINI=/esg/config/esgcet/esg.ini
+myproxy-logon -s esg-idx.demonet.local -l testpub -o /root/.globus/certificate-file
+source /usr/local/conda/bin/activate esgf-pub
+esgprep mapfile --mapfile test.map --project cordex /esg/data/datapool1
+esgpublish --map mapfiles/test.map --project cordex --new-version `date +%Y%m%d` --service fileservice
+esgpublish --map mapfiles/test.map --project cordex --new-version `date +%Y%m%d` --noscan --thredds
+esgpublish --map mapfiles/test.map --project cordex --new-version `date +%Y%m%d` --noscan --publish
