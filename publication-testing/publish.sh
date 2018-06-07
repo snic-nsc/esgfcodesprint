c3286e0ec8bc0965f270ffa975894b82837d6750
Prashanth Dwarakanath <pchengi@nsc.liu.se>
2018-06-07 16:55:53 +0000

    update publication scripts to set uvcdat log to false

--- publication-testing/publish.sh
+++ publication-testing/publish.sh
@@ -2,6 +2,7 @@ mkdir -p /root/.globus
 rm -rf /root/.globus/certificates
 ln -s /etc/grid-security/certificates /root/.globus/certificates
 export ESGINI=/esg/config/esgcet/esg.ini
+export UVCDAT_ANONYMOUS_LOG=false
 myproxy-logon -s esg-idx.demonet.local -l testpub -o /root/.globus/certificate-file
 source /usr/local/conda/bin/activate esgf-pub
 esgmapfile --mapfile test.map --project cordex /esg/data/datapool1
