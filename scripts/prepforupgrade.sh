fd87dac1fd4c1d92be452d4dec4a16acd48a2605
Prashanth Dwarakanath <pchengi@nsc.liu.se>
2018-08-08 16:52:01 +0200

    update

--- scripts/prepforupgrade.sh
+++ scripts/prepforupgrade.sh
@@ -21,6 +21,18 @@ esg-node stop
 rm -rf /usr/local/tomcat/webapps/*
 rm -rf /usr/local/src
 
+res=`esg-node --version|grep Version|awk {'print $2'}|cut -d '-' -f1|cut -d 'v' -f2`;
+tofix=0
+if echo $res|grep '2.6' >/dev/null; then
+    tofix=1;
+    if echo $res|grep '2.6.9' >/dev/null; then
+        tofix=0;
+    fi
+fi
+if [ $tofix -eq 1 ]; then
+    script -c 'bash tempfix' tempfix_log
+fi
+
 if [ $devel -eq 1 ]; then
 	wget -O esg-bootstrap.liu http://esg-dn2.nsc.liu.se/esgf/dist/devel/$release_maj_version/$release_sub_version/esgf-installer/esg-bootstrap.liu --no-check-certificate
 	chmod 555 esg-bootstrap.liu
